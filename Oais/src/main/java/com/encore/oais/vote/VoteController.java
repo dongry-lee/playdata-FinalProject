28cdee7564adc517172761501586500c8cfa68ed
YJS <yjs6156@naver.com>
2022-08-27 11:59:28 +0900

    JS
    VoteController 수정

--- Oais/src/main/java/com/encore/oais/vote/VoteController.java
+++ Oais/src/main/java/com/encore/oais/vote/VoteController.java
@@ -1,27 +1,39 @@
 
-//package com.encore.oais.vote;
-//
-//import com.encore.oais.voteboard.VoteBoard;
-//import com.encore.oais.voteboard.VoteBoardService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//@Controller
-//public class VoteController {
-//    @Autowired
-//    private VoteService voteService;
-//
-//    @GetMapping("/voteboard/vote")
-//    public String VoteBoardFrom(){
-//        return "vote/vote";
-//
-//    }
-//    @PostMapping("/voteboard/votepro")
-//    public String boardvote(Vote vote) {
-//        voteService.writevote(vote);
-//
-//        return "";
-//    }
-//
+package com.encore.oais.vote;
+
+import com.encore.oais.voteboard.VoteBoard;
+import com.encore.oais.voteboard.VoteBoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class VoteController {
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @GetMapping("/voteboard/vote")
+    public String newVoteFrom() {
+        return "/vote/vote";
+
+    }
+
+    @PostMapping("/voteboard/create")
+    public String createVote(VoteDto dto) {
+        System.out.println(dto.toString());
+
+
+        Vote vote = dto.toEntity();
+        System.out.println(vote.toString());
+
+
+        Vote saved = voteRepository.save(vote);
+        System.out.println(saved.toString());
+        return "";
+
+    }
+
+
+}
+
--- Oais/src/main/java/com/encore/oais/vote/VoteRepository.java
+++ Oais/src/main/java/com/encore/oais/vote/VoteRepository.java
@@ -2,12 +2,13 @@ package com.encore.oais.vote;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Id;
 import java.util.Optional;
 
-
+@Repository
 public interface VoteRepository extends JpaRepository<Vote,Integer> {
 
 }
