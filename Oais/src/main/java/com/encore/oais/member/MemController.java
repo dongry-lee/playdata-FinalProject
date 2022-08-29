daf45fe0fcf24aa3a4c2132191a64e4eb300da8f
dorumamu <272518@naver.com>
2022-08-29 12:34:20 +0900

    Mem controller mypageForm 연결 index.jsp에 로그인 시 헤더에 mypage 연결 버튼 생성 mypageForm.jsp 파일 생성

--- Oais/src/main/java/com/encore/oais/member/MemController.java
+++ Oais/src/main/java/com/encore/oais/member/MemController.java
@@ -122,6 +122,10 @@ public class MemController {
     //회원가입
     //로그인
     //내정보확인 및 수정
+    @GetMapping("/mypage")
+    public String mypageForm(){
+        return "member/mypageForm";
+    }
     //로그아웃 / 탈퇴
 }
 
